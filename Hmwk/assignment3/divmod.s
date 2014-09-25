7bc26ed5f96238a465b4d42d736a812bc74377a8
ajuanr <j_ruiz05@yahoo.com>
2014-09-25 10:08:33 -0700

    Finished efficient divmod assignment

--- Hmwk/assignment3/divmod.s
+++ Hmwk/assignment3/divmod.s
@@ -6,9 +6,9 @@
     @ declare variables
      .global main
 main:
-    mov r2, #0       @ var a
-    mov r3, #4         @ var b
-    mov r4, #0         @ flag: 0=a/b  or  1=a%b
+    mov r2, #111       @ var a
+    mov r3, #9         @ var b
+    mov r4, #1         @ flag: 0=a/b  or  1=a%b
     mov r6, #0         @ s -> r6 has the present scale 10^
     mov r7, #0         @ sf -> r7 scale factor = r3*r6 to subtract
     mov r8, #10        @ factor -> r8 shift factor 10
