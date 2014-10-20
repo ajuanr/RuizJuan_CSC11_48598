cfc459fbfbe2461ac2616706da83b283c57e63e9
Juan Ruiz <j.ruiz3750@gmail.com>
2014-10-20 12:20:32 -0700

    Signed-off-by: Juan Ruiz <j.ruiz3750@gmail.com>

--- Midterm/C code/main.cpp
+++ Midterm/C code/main.cpp
@@ -9,11 +9,49 @@
 #include <iostream>
 using namespace std;
 
+void problem1();
+
 /*
  * 
  */
 int main(int argc, char** argv) {
-    cout << "Hello World\n";
+    problem1();
     return 0;
 }
 
+void problem1() {
+    cout << "Enter the pay rate and hours worked: ";
+    int r0=0;             // total pay
+    int r1;               // hours worked
+    int r2;               // pay rate
+    int r3;               // temp
+    cin >> r2 >> r1;
+    cout << "Hours: " << r1 << endl;
+    cout << "Pay rate: " << r2 << endl;
+    
+    // check if triple time applies
+    if ( r1 > 40) {
+        // r3 = r1-20;             // holds number of hours over 40
+        r1 = r1 - 20;           // shift hours into double time
+        r3 = r1 * r2*3;           //r3 now holds that amount of triple time pay
+        r0 = r0 + r3;
+        cout << r0 << endl;
+    }
+    
+    // check if double time applies
+    if ( r1 > 20) {
+        //r3 = r1-20;             // holds number of hours over 40
+        r1 = r1 - 20;           // shift hours into straight time
+        r3 = r1 * r2*2;           //r3 now holds that amount of triple time pay
+        r0 = r0 + r3;
+        cout << r0 << endl;
+    }
+    // check if straight time applies
+    if ( r1 > 0) {
+        r3 = r1 * r2;
+        r0 = r0 + r3;
+    }
+    cout << "Pay: ";
+    cout << r0 << endl;
+    
+}
