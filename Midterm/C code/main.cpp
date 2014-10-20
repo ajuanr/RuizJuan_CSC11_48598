06cd84cd3a8e5792078fc22a0f73a9cdfb046353
Juan Ruiz <j.ruiz3750@gmail.com>
2014-10-20 11:45:37 -0700

    Signed-off-by: Juan Ruiz <j.ruiz3750@gmail.com>

---
+++ Midterm/C code/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on October 20, 2014, 11:43 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout << "Hello World\n";
+    return 0;
+}
+
