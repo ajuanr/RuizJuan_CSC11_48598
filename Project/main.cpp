51b256b21a0e4cd49bad68cc0c04cb3f4a75f58c
Juan Ruiz <j_ruiz05@yahoo.com>
2014-10-27 07:50:00 -0700

    Signed off by Juan ruiz

---
+++ Project/main.cpp
@@ -0,0 +1,21 @@
+/*
+ *
+ * File:   main.cpp
+ * Author: Juan Ruiz
+ * Project 1 - 48598
+ *
+ * Created on October 27, 2014, 11:43 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+
+/*
+ *
+ */
+int main(int argc, char** argv) {
+    cout << "Hello, World\n";
+    return 0;
+}
