10518c6791d225a6dbc19c343eaada63d696b31b
ajuanr <j_ruiz05@yahoo.com>
2014-10-31 23:13:46 +0000

    Signed off by Juan Ruiz

--- Project/fillArray.s
+++ Project/fillArray.s
@@ -5,7 +5,7 @@ master: .skip 12
 
 /* To check values in array */
 .balign 4
-num: .asciz "%d"
+numRead: .asciz "%d, "
 
 /* Message to user */
 .balign 4
@@ -22,14 +22,27 @@ main:
 
    ldr r1, address_of_master
    
+
+   /* fill the array */
    mov r2, #0
-   loop:
-   cmp r2, #3
-   beq exit
-   
-   str r2, [r1, +r2, lsl #1]
-   add r2, #1
-   bal loop
+   fillLoop:
+       cmp r2, #3
+       beq exit
+       str r2, [r1], +#4       /* r1+r2*4 <- r2 */
+       add r2, #1
+       bal fillLoop
+
+   /* print the elements */
+   ldr r0, address_of_num
+   @sub r1, r2, lsl #2
+   mov r2, #0
+   printLoop: 
+       cmp r2, #3
+       beq exit
+       mov r1, #7
+       bl printf
+       add r2, #1
+       bal printLoop
 
    exit:
    add sp, #4
@@ -39,3 +52,4 @@ main:
 
 address_of_master: .word master
 address_of_message: .word message
+address_of_num: .word numRead
