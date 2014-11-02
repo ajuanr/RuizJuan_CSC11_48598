b6fa39aab5e14ce6c6046410ebdb7e903b927f66
ajuanr <j_ruiz05@yahoo.com>
2014-11-02 05:21:09 +0000

    Signed off by Juan Ruiz

--- Project/fillArray.s
+++ Project/fillArray.s
@@ -4,28 +4,46 @@
 a: .skip 12
 
 /* To check values in array */
-.balign 4
 numRead: .asciz "%d, "
 
 /* Message to user */
-.balign 4
 message: .asciz "array contains 3 elements\n"
 
 .text
+
+   /* fill the array */
+   fill:
+       push {lr}
+       /* Save the array */
+       ldr r1, address_of_a         /* r1 <- master */
+       mov r2, #0
+         ldr r0, address_of_message
+   fillLoop:
+       cmp r2, #3                   /* have we reached 3 yet */
+       beq exit_fill
+       add r2, r2, #1
+       str r2, [r1, r2, lsl #2]     /* (*r1  ← r2) + #4 */
+         bl printf
+       bal fillLoop
+   
+       /* Exit the function */    
+       exit_fill:
+         pop {lr}
+         bx lr
+
+
    /* print the array elements */
    print:
-      push {lr}
-      mov r2, #0
-      printLoop:
+     push {lr}
+     mov r2, #0
+     mov r1, r2
+     printLoop:
          cmp r2, #3
-         beq end
-         ldr r0, address_of_numRead
-         mov r1, r2
-         bl printf
+         beq exit_print
          add r2, r2, #1
-@         bal printLoop
+         bal printLoop
     /* exit print function */
-    end:
+    exit_print:
     pop {lr}
     bx lr
 
@@ -34,27 +52,12 @@ main:
    /* Save the link registers */
    push {lr}
 
-   /* Prompt for user */
-   ldr r0, address_of_message
-   bl printf
 
-   /* Save the array */
-   ldr r1, address_of_a       /* r1 <- master */
+   bl fill
+   @bl print
 
-   /* fill the array */
-   mov r2, #0
-   fillLoop:
-       cmp r2, #3            /* have we reached 3 yet */
-       beq exit              /* r2 == 3 */
-       str r2, [r1, r2, lsl #2]     /* (*r1  ← r2) + #4 */
-       add r2, r2, #1
-       bal fillLoop
-
-
-   exit:
-   bl print
-       pop {lr} 
-       bx lr
+   pop {lr} 
+   bx lr
 
 address_of_a: .word a
 address_of_message: .word message
