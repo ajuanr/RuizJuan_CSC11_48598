d340aef22741f940b936f6d7562aef908818f4c4
ajuanr <j_ruiz05@yahoo.com>
2014-11-02 09:02:20 -0800

    Signed off by Juan Ruiz

--- Project/arrays.s
+++ Project/arrays.s
@@ -4,60 +4,81 @@
 a: .skip 12
 
 /* To check values in array */
-numRead: .asciz "%d, "
+numRead: .asciz "%d\n"
 
 /* Message to user */
 message: .asciz "This step is working\n"
 
 .text
+/* Generate a random number */
+random:
+    push {lr}                 /* Push lr onto the top of the stack */
+	
+        bl rand                      /* Call rand */
+        mov r1,r0,ASR #1             /* In case random return is negative */
+        mov r2,#8                   /* Move 8 to r2 */
+		                         /* We want rand()%8 so cal divMod with rand()%8 */
+	bl divMod                    /* Call divMod function to get remainder */
+	
+        pop {lr}                     /* Pop the top of the stack and put it in lr */
+        bx lr                        /* Leave main */
+/* Exit random number generator */
 
-   /* fill the array */
-   fill:
-       push {lr}
-       /* Save the array */
-       ldr r1, address_of_a         /* r1 <- master */
-       mov r2, #0
-   fillLoop:
-       cmp r2, #3                   /* have we reached 3 yet */
-       beq exit_fill
-       add r2, r2, #1
-       str r2, [r1, r2, lsl #2]     /* (*r1  ← r2) + #4 */
-       bal fillLoop
+/* fill the array */
+fill:
+    push {lr}
+    /* Save the array */
+    ldr r1, address_of_a         /* r1 <- master */
+    mov r2, #0
+    fillLoop:
+        cmp r2, #3                   /* have we reached 3 yet */
+        beq exit_fill
+        mov r3, r2
+        @bl random
+        str r2, [r1, r2, lsl #2]     /* (*r1  ← r2) + #4 */
+        mov r2, r3
+        add r2, r2, #1
+        bal fillLoop
    
-       /* Exit the function */    
-   exit_fill:
+    /* Exit the function */    
+    exit_fill:
       pop {lr}
       bx lr
 
 
-   /* print the array elements */
-   print:
-     push {lr}
-     ldr r2, address_of_a
-     mov r3, #0
-     printLoop:
-        cmp r3, #3
-        beq exit_print
-        ldr r1, [r2, r3, lsl #2]
-        add r3, r3, #1
-        bal printLoop
+/* print the array elements */
+print:
+  push {lr}
+  ldr r2, address_of_a
+  mov r3, #0
+  printLoop:
+     cmp r3, #3
+     beq exit_print
+     ldr r1, [r2, r3, lsl #2]
+     add r3, r3, #1
+     bal printLoop
 
-   /* exit print function */
-   exit_print:
+  /* exit print function */
+  exit_print:
        pop {lr}
        bx lr
 
-    .global main
+.global main
 main:
    /* Save the link registers */
    push {lr}
+
+    /* for random number */
+	mov r0,#0                    /* Set time(0) */
+    bl time                      /* Call time */
+	bl srand                     /* Call srand */
 
    bl fill
    bl print
    
    ldr r0, address_of_numRead
    ldr r1, address_of_a
-   ldr r1, [r1, +#8]
+   ldr r1, [r1, +#0]
    bl printf
 
    pop {lr} 
@@ -67,4 +88,8 @@ address_of_a: .word a
 address_of_message: .word message
 address_of_numRead: .word numRead
 
+/* External functions */
 .global printf
+.global time
+.global srand
+.global rand
