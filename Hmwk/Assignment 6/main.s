bf16f6c510e90b3486a70199ff3bf57d899bca61
ajuanr <j_ruiz05@yahoo.com>
2014-11-05 17:54:52 +0000

    Signed off by Juan Ruiz

--- Hmwk/Assignment 6/main.s
+++ Hmwk/Assignment 6/main.s
@@ -8,7 +8,7 @@ beg: .word 0
 end: .word 0
 
 /* Tell user how long function took */
-message: .asciz "The collatz function took %d seconds to run\n"
+message: .asciz "Collatz function\n"
 
 /* Tell user time at function call */
 cur_time: .asciz "The current time is: %d\n"
@@ -17,7 +17,6 @@ cur_time: .asciz "The current time is: %d\n"
     .global main
 main:
     push {lr}
-
 
     /* Get initial time */
     mov r0, #0
@@ -32,7 +31,30 @@ main:
     add r0, r0, lsl #29
     bl collatz
 
+    ldr r0, ad_of_message
+    bl printf
+
+    /* Get final time */
+    mov r0, #0
+    bl time
     mov r1, r0
+    ldr r0, ad_of_cur_time
+    bl printf
+
+
+    /* Get initial time */
+    mov r0, #0
+    bl time
+    mov r1, r0
+    mov r2, r0
+    ldr r0, ad_of_cur_time
+    bl printf
+
+    /* Call predicated collatz function */
+    mov r0, #1
+    add r0, r0, lsl #29
+    bl collatz2
+
     ldr r0, ad_of_message
     bl printf
 
@@ -44,33 +66,6 @@ main:
     ldr r0, ad_of_cur_time
     bl printf
 
-/*
-
-    /* call collatz function 
-    mov r0, #1
-    mov r5, #0
-    mov r4, #1
-    add r4, r4, r4, lsl #30  /* r4 <- 2^20 
-    loop:
-        mov r0, #100
-        cmp r5, r4
-@        bl collatz
-        addne r5, r5, #1
-        bne loop
-
-    /* Get final time 
-    mov r0, #0
-    bl time
-    mov r1, r0
-    ldr r0, ad_of_message
-    bl printf
-
-
-    /* print message to user 
-    sub r1, r2, r1
-    ldr r0, ad_of_message
-    bl printf
-*/
     pop {lr}
     bx lr
      
