c4071fe19cfa22e0ce0031d213af19b51d04e13d
ajuanr <j_ruiz05@yahoo.com>
2014-11-14 22:47:37 +0000

    Signed off by Juan Ruiz

--- Hmwk/Assignment 7a/main.s
+++ Hmwk/Assignment 7a/main.s
@@ -1,29 +1,29 @@
-/* Ask user for temp prompt */
-prompt: asciz "Enter the temperature in degrees farenheit\n"
-
-/* scan format */
-.balign 4
+.data
+prompt: .asciz "Enter temperature in degrees Farenheit :"
 format: .asciz "%d"
+result: .asciz "Temp is %d\n"
 
-/* Result */
-result: "Temp is %d\n"
-
-
+.text
 .global main
-
 main:
-    push {lr}      /* save the link registers */
+    push {lr}      /* save the link register */
 
     ldr r0, ad_of_prompt
     bl printf
 
-    push {r1}      /* push r1 onto stack before read */
+    sub sp, sp, #4     /* make room on stack for read */
+    mov r1, sp      /* push r1 onto stack before read */
+    ldr r0, ad_of_format
     bl scanf
 
-    ldr r1, sp
+    ldr r1, [sp]
     ldr r0, ad_of_result 
     bl printf
+    add sp, sp, #4
+    pop {lr}
+    bx lr
 
 
 ad_of_prompt: .word prompt
+ad_of_format: .word format
 ad_of_result: .word result
