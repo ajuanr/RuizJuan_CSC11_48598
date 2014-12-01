6c4e9180914ee66a9f3b600a660026b0eb538f49
ajuanr <j_ruiz05@yahoo.com>
2014-12-01 22:39:06 +0000

    Signed off by Juan Ruiz

--- Hmwk/Assignment 7c/main.s
+++ Hmwk/Assignment 7c/main.s
@@ -6,11 +6,16 @@
 prompt: .asciz "Enter temperature from 32-212 in degrees Farenheit: "
 format: .asciz "%f"
 result: .asciz "Temp is %f\n"
-initial: .word 0
+delta: .asciz "The delta time is: %d seconds\n"
+initial: .float 0
 
 thirtyTwo: .float 32.0
 five: .float 5.0
 nine: .float 9.0
+
+/* For delta time for n number of loops */
+beg: .word 0
+end: .word 0
 
 .global main
 main:
@@ -27,19 +32,26 @@ main:
     ldr r1, ad_of_initial   /* put the value in floating point register */
     vldr s14, [r1]
 
-    ldr r1, ad_of_thirtyTwo /* Get registers ready to put into floaing point*/
+    ldr r1, ad_of_thirtyTwo /* Get registers ready to put into floating point*/
     ldr r2, =five
     ldr r3, =nine
 
-    vldr s15, =32 @[r1]          /* Load the registers */
-    vcvt.f32.s32 s15, s15
+    vldr s15, [r1]          /* Load the registers */
     vldr s16, [r2]
     vldr s17, [r3]
+
+    mov r0, #0             /* get the initial time */
+    ldr r1, =beg
+    str r0, [r1]
 
     vsub.f32 s14, s14, s15  /* This is (F-32) as part of conversion */
     vmul.f32 s14, s14, s16
     vdiv.f32 s14, s14, s17
     
+
+    mov r0, #0              /* get final time */
+    ldr r1, =end
+    str r0, [r1]    
 
     vcvt.f64.f32 d0, s14    /* convert to double for printing  */
   
