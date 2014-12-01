6d30ad96eec4b23595403c8d8d627d8118049d53
ajuanr <j_ruiz05@yahoo.com>
2014-12-01 12:03:40 -0800

    Signed off by Juan Ruiz

--- Hmwk/Assignment 7c/main.s
+++ Hmwk/Assignment 7c/main.s
@@ -9,6 +9,8 @@ result: .asciz "Temp is %f\n"
 initial: .word 0
 
 thirtyTwo: .float 32.0
+five: .float 5.0
+nine: .float 9.0
 
 .global main
 main:
@@ -25,9 +27,18 @@ main:
     ldr r1, ad_of_initial   /* put the value in floating point register */
     vldr s14, [r1]
 
-    vldr s15, ad_of_thirtyTwo
+    ldr r1, ad_of_thirtyTwo /* Get registers ready to put into floaing point*/
+    ldr r2, =five
+    ldr r3, =nine
+
+    vldr s15, [r1]          /* Load the registers */
+    vldr s16, [r2]
+    vldr s17, [r3]
+
+    vsub.f32 s14, s14, s15  /* This is (F-32) as part of conversion */
+    vmul.f32 s14, s14, s16
+    vdiv.f32 s14, s14, s17
     
-    vadd.f32 s14, s14, s15
 
     vcvt.f64.f32 d0, s14    /* convert to double for printing  */
   
