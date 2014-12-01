757b9398337ab1cde6c08c176a77afaa3c03f401
ajuanr <j_ruiz05@yahoo.com>
2014-12-01 11:52:05 -0800

    Signed off by Juan Ruiz

--- Hmwk/Assignment 7c/main.s
+++ Hmwk/Assignment 7c/main.s
@@ -8,11 +8,12 @@ format: .asciz "%f"
 result: .asciz "Temp is %f\n"
 initial: .word 0
 
-/* thrtyTwo */
 thirtyTwo: .float 32.0
 
 .global main
 main:
+    push {lr}
+    sub sp, sp, #4          /* 8-byte align the stack */
 
     ldr r0, ad_of_prompt    /* print message to user */
     bl printf
@@ -24,10 +25,9 @@ main:
     ldr r1, ad_of_initial   /* put the value in floating point register */
     vldr s14, [r1]
 
-
-    vldr s15, =thirtyTwo
+    vldr s15, ad_of_thirtyTwo
     
-    vsub.f32  s14, s14, s15
+    vadd.f32 s14, s14, s15
 
     vcvt.f64.f32 d0, s14    /* convert to double for printing  */
   
@@ -35,9 +35,11 @@ main:
     vmov r2, r3, d0
     bl printf
 
-    mov r7, #1              /* Exit */
-    swi 0 
+    add sp, sp, #4         /* restore the stack */
+    pop {lr}               /* and exit */
+    bx lr
 
 ad_of_prompt: .word prompt
 ad_of_format: .word format
 ad_of_initial: .word initial
+ad_of_thirtyTwo: .word thirtyTwo
