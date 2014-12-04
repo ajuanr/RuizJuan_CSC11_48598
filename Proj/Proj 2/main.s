035260ec9354a3a976724cf159a3d2d2ae310d81
ajuanr <j_ruiz05@yahoo.com>
2014-12-04 07:48:27 -0800

    Signed off by Juan Ruiz

--- Proj/Proj 2/main.s
+++ Proj/Proj 2/main.s
@@ -3,5 +3,24 @@
  * Ruiz, Juan - Project 2 - 48598
 */
 
+.data 
+/* arrays holding the hand of the player and dealer 
+ * max number of card with one deck is 11: A A A A 2 2 2 2 3 3 3 = 21
+ * array padded with room for three more cards/
+*/
 
+.balign 4
+dealer: .skip 56
 
+.balign 4
+player: .skip 56
+
+/* this array holds the value of the 52 cards in the deck */
+.balign 4
+cardVal: .skip 220
+
+/* this array holds the index of the next card to be dealt */
+.balign 4
+shuflIndx: .skip 220
+
+.text global
