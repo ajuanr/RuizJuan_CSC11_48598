39147e18e3f89a1c13eed704465bb0fd5fa3438f
ajuanr <j_ruiz05@yahoo.com>
2014-12-04 23:19:59 +0000

    Signed off by Juan Ruiz

--- Proj/Proj 2/random.s
+++ Proj/Proj 2/random.s
@@ -4,17 +4,18 @@
 random:
     push {r4, r5, r6, lr}
 
-    mov r4,r1                         /* Save r1 to r2 */
-	                              /* We want rand()%8 */
-    mov r5, r0
+    mov r4,r0                         /* Save min in r4 */
+    mov r5, r1                        /* Save max in r5 */
 
     bl rand                           /* Call rand */
     
     mov r1, r0, asr #1                /* In case random return is negative */
-    mov r2, r1
+    mov r2, r5
     bl divMod                         /* Call divMod function to get remainder */
+                                          /* divmod returns in r1, r1 */
 
-    mov r0, r1                        /* return result in r0 */
+    mov r0, r2                        /* return result in r0 */
+    add r0, r0, r4
 
     pop {r4, r5, r6, lr}                      /* Pop the top of the stack and put it in lr */
     bx lr                             /* Leave main */
